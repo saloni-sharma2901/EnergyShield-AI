78095396e5ef0b34db3087bb2a2ad4fe045fd753
saloni-sharma2901 <saloni.sharma8587@gmail.com>
2026-08-25 00:24:27 +0530

    Update AppRoutes.jsx

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -4,7 +4,7 @@ import Home from "../pages/Home";
 import About from "../pages/About";
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
-import Register from "../pages/Register";
+import Register from "../pages/Register.jsx";
 
 function AppRoutes() {
     return (
